26975c0b3867ddce8e1216628943bdbd668ed2dd
Catalin40479296 <40479296@live.napier.ac.uk>
2021-09-23 19:18:27 +0100

    secondCommit

---
+++ exercise1.py
@@ -0,0 +1,6 @@
+import random
+
+randomWord1 = ["like", "love", "enjoy", "dont know", "want to learn"]
+randomWord2 = ["Simon", "books", "tutorials", "online courses"] 
+print("I " + random.choice(randomWord1) + " Python3 <br> I will learn it from " + random.choice(randomWord2))
+
